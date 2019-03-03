d7b8a1c1d72065270fc2b7f788af1af399f5f297
mccabmic <mccabmic@oregonstate.edu>
2019-03-02 21:56:19 -0800

    refactor, utils

--- smallsh.c
+++ smallsh.c
@@ -1,56 +1,37 @@
 #define _GNU_SOURCE
 
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <signal.h>
+#include "util.h"
 
-#define BUFFER 2048
-#define NAME_MAX 255
-#define MAX_ARGS 256
-#define PID_LIM 80
+// Globals
+pid_t bg_pid[100];
+int num_bg_process = 0;
 
-struct command {
-	char command[NAME_MAX];
-	char* args[MAX_ARGS];
-	int argc;
-	char input[NAME_MAX];
-	char output[NAME_MAX];
-	bool bg;
-};
-
-
-void shell_process();
-struct command* parse(char* string);
-int sh_exit();
-void print_command(struct command*);
-void sh_status();
-int handle_command(struct command* cmd);
-void sh_chdir(struct command* cmd);
-void sh_launch(struct command* cmd);
-char* expand(char* string, char* pattern);
-void sh_reap();
-
+// Signal Globals
 volatile bool foreground_mode = false;
 volatile bool signaled = false;
-
 int bg_status = 0;
 
+// Shell Prototypes
+int sh_built_in(struct command* cmd);
+
+void sh_process();
+void sh_status();
+void sh_chdir(struct command* cmd);
+void sh_launch(struct command* cmd);
+void sh_reap();
+void sh_fgmode();
+
+// Signal Handler for TSTP
 static void sig_stp(int sig){
 	signaled = !signaled;
 	foreground_mode = !foreground_mode;
 }
 
 int main(){
-	shell_process();
+	sh_process();
 }
 
-void fg_mode(){
+void sh_fgmode(){
 	if (!signaled){
 		return;
 	}
@@ -65,15 +46,12 @@ void fg_mode(){
 }
 void sh_reap(){
 	// loop through all pids and kill them
+	for (int i = 0; i < num_bg_process; i++){
+		kill(bg_pid[i], SIGKILL);
+	}
 }
 
-void shell_process(){
-	/* listen for commands
-	* when a non-built in command is received, fork()
-	* this child will do I/O redirection before execing the command given	
-	* when handling redirection ,just pass ls into exec, so get rid of redir symbol and des/source
-	*/
-	
+void sh_process(){
 	bool loop = true;
 	struct sigaction SIGINT_action = {0};
 	struct sigaction ignore_action = {0};
@@ -87,83 +65,80 @@ void shell_process(){
 	SIGINT_action.sa_handler = SIG_DFL;
 	ignore_action.sa_handler = SIG_IGN;
 	
-	sigaction(SIGINT, &ignore_action, NULL);
-	sigaction(SIGTSTP, &SIGSTP_action, NULL);
-
 	while(loop){
+		sigaction(SIGINT, &ignore_action, NULL);
+		sigaction(SIGTSTP, &SIGSTP_action, NULL);		
 		
-		sigaction(SIGTSTP, &SIGSTP_action, NULL);
-		// Prompt Variables
-		char *line = NULL;
-		size_t len = BUFFER;
-		ssize_t nread;
-		
-		fg_mode();
-
+		sh_fgmode();
 		if(signaled){
 			signaled = false;
 		}
 
 		printf(": ");
 		fflush(stdout);
+		
+		char *line = NULL;
+		size_t len = BUFFER;
+		ssize_t nread;
 
 		while((nread = getline(&line, &len, stdin)) == -1){
 			clearerr(stdin);
 		}
 
 		line = expand(line, "$$");
+		struct command* cmd = parse(line);
+		int branch = sh_built_in(cmd);
+		//print_command(cmd);
 		
-		// Build-in function, exit
-		if (strcmp(line, "exit\n") == 0){
+		// Exit
+		if (branch == 2){
 			free(line);
+			free(cmd);
 			break;
 		}
-
-		// Built-in function status
-		else if (strcmp(line, "status\n") == 0 || strcmp(line, "status &\n") == 0){
-			sh_status();	
+		
+		// Unhandled Command
+		if (branch == 0){		
+			if (foreground_mode){cmd->bg = false;}
+			pid_t spawnid;
+			spawnid = fork();
+			bg_pid[num_bg_process] = spawnid;
+			num_bg_process += 1;
+			switch(spawnid){
+				// Failure to fork
+				case -1:
+					perror("Fork failed!");
+					exit(1);
+				break;
+				// Child
+				case 0:
+					sigaction(SIGTSTP, &ignore_action, NULL);
+					if(cmd->bg != true){
+						sigaction(SIGINT, &SIGINT_action, NULL);
+					}
+					sh_launch(cmd);
+				break;
+				// Parent
+				default:
+					// Child is running in foreground
+					if (cmd->bg != true){
+						waitpid(spawnid, &bg_status, 0);
+						if(WIFSIGNALED(bg_status)){
+							sh_status();
+						}
+					}
+					// Child is running in background
+					else{
+						printf("background process %d has started\n", spawnid);
+						fflush(stdout);
+						waitpid(spawnid, &bg_status, WNOHANG);
+					}
+				break;						
+			}
 		}
 		
-		else{
-			struct command* cmd = parse(line);
-			if (foreground_mode){cmd->bg = false;}
-			
-			if(handle_command(cmd)){
-				pid_t spawnid;
-				spawnid = fork();
-				switch(spawnid){
-					case -1:
-						perror("Fork failed!");
-						exit(1); break;
-					case 0:
-						 sigaction(SIGTSTP, &ignore_action, NULL);
-						 if(!cmd->bg){
-								sigaction(SIGINT, &SIGINT_action, NULL);
-						 }
-						 sh_launch(cmd);
-						 break;
-					default:
-					 	// Foreground
-						if (cmd->bg != true){
-							waitpid(spawnid, &bg_status, 0);
-							if(WIFSIGNALED(bg_status)){
-								sh_status();
-							}
-							fflush(stdout);
-						}
-						
-						// Background
-						else{
-							printf("background process %d has started\n", spawnid);
-							fflush(stdout);
-							waitpid(spawnid, &bg_status, WNOHANG);
-						}
-						break;						
-				}
-			}
-			free(cmd); // what if parent frees command before child gets to it?
-		}
-
+		free(line);
+		free(cmd);
 		// REAP THE ZOMBIE CHILDRENZ
 		pid_t zombies = waitpid(-1, &bg_status, WNOHANG);
 		while (zombies > 0){
@@ -172,9 +147,32 @@ void shell_process(){
 			sh_status();
 			zombies = waitpid(-1, &bg_status, WNOHANG);
 		}
-		free(line);
+	} // end while loop
+} //end sh process
+
+int sh_built_in (struct command* cmd){
+	if(strcmp(cmd->command, "BLANK") == 0 || strcmp(cmd->command, "COMMENT") == 0){
+		return 1;
 	}
+
+	if(strcmp(cmd->command, "exit") == 0){
+		sh_reap();
+		return 2;
+	}
+	
+	if(strcmp(cmd->command, "status") == 0 || strcmp(cmd->command, "status &") == 0){ 
+		sh_status();
+		return 1;
+	}
+
+	if(strcmp(cmd->command, "cd") == 0){
+		sh_chdir(cmd);
+		return 1;
+	}
+
+	return 0;
 }
+
 void sh_launch(struct command* cmd){
 	int result, input, output;
 	if (strcmp(cmd->input, "") != 0){
@@ -208,18 +206,6 @@ void sh_launch(struct command* cmd){
 
 }
 
-int handle_command (struct command* cmd){
-	if(strcmp(cmd->command, "BLANK") == 0 || strcmp(cmd->command, "COMMENT") == 0){
-		return 0;
-	}
-	
-	if (strcmp(cmd->command, "cd") == 0){
-		sh_chdir(cmd);
-		return 0;
-	}
-	return 1;
-}
-
 void sh_chdir(struct command* cmd){
 	if (cmd->argc == 1){
 		chdir(getenv("HOME"));
@@ -249,100 +235,5 @@ void sh_status(){
 		printf("unknown error in __FILE__\n");
 		fflush(stdout);
 	}
-}
-
-void print_command(struct command* cmd){
-	printf("command: %s\n", cmd->command);
-	printf("output: %s\n", cmd->output);
-	printf("input: %s\n", cmd->input);
-	printf("background: %d\n", cmd->bg);
-	printf("args: "); for (int i = 0; cmd->args[i] != NULL; i++) printf("%s ", cmd->args[i]);
-	printf("\n");
-}
-
-//https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
-char* expand(char* string, char* pattern){
-	
-	char pid_string[PID_LIM] = {0};
-	sprintf(pid_string, "%ld", (long)getpid());
-
-	size_t new_string_size = strlen(string) + 1;
-	char* new_string = malloc(new_string_size);
-	size_t offset = 0;
-	
-	char* delim;
-	char* in = string;
-	
-	while (delim = strstr(in, pattern)){
-		memcpy(new_string + offset, in, delim - in);
-		offset += delim - in;
-
-		in = delim + strlen(pattern);
-		
-		new_string_size = new_string_size - strlen(pattern) + strlen(pid_string);
-		new_string = realloc(new_string, new_string_size);
-
-		memcpy(new_string + offset, pid_string, strlen(pid_string));
-		offset += strlen(pid_string);
-	}
-
-	strcpy(new_string + offset, in);
-	
-	free(string);
-	return new_string;
-		
-}
-
-struct command* parse(char string[BUFFER]){
-	const char delimit[] = " \t\r\n\v\f";
-	struct command* cmd = malloc(sizeof(struct command));
-	if (string[0] == '\n'){
-		snprintf(cmd->command, sizeof(cmd->command), "BLANK");
-		return cmd;
-	}
-	else if (string[0] == '#'){
-		snprintf(cmd->command, sizeof(cmd->command), "COMMENT");
-		return cmd;
-	}
-
-	// Init Struct
-	snprintf(cmd->input, sizeof(cmd->input), "");
-	snprintf(cmd->output, sizeof(cmd->output), "");
-	cmd->bg = false;
-	
-	char* token;
-	// Set command
-	token = strtok(string, delimit);
-	snprintf(cmd->command, sizeof(cmd->command), token);
-	
-	size_t arg_count = 0;
-	// Start setting loop
-	while(token != NULL){
-		if (strcmp(token, "<") == 0){
-			snprintf(cmd->input, sizeof(cmd->input), strtok(NULL, delimit));
-		}
-		else if (strcmp(token, ">") == 0){
-			snprintf(cmd->output, sizeof(cmd->output), strtok(NULL, delimit));
-		}
-		else{
-			cmd->args[arg_count] = token;
-			arg_count += 1;
-		}
-		token = strtok(NULL, delimit);
-	}
-	cmd->argc = arg_count;
-	if(strcmp(cmd->args[arg_count - 1],"&") == 0){
-		cmd->args[arg_count -1] = '\0';
-		cmd->bg = true;
-		if(strcmp(cmd->input, "") == 0){
-			snprintf(cmd->input, sizeof(cmd->input), "/dev/null");
-		}
-		if(strcmp(cmd->output, "") == 0){
-			snprintf(cmd->output, sizeof(cmd->output), "/dev/null");
-		}
-	
-	}
-	cmd->args[arg_count] = NULL;
-	return cmd;
 }
 
---
+++ util.c
@@ -0,0 +1,94 @@
+#include "util.h"
+
+struct command* parse(char string[BUFFER]){
+	const char delimit[] = " \t\r\n\v\f";
+	struct command* cmd = malloc(sizeof(struct command));
+	if (string[0] == '\n'){
+		memcpy(cmd->command, "BLANK", sizeof(cmd->command));
+		return cmd;
+	}
+	else if (string[0] == '#'){
+		memcpy(cmd->command, "COMMENT", sizeof(cmd->command));
+		return cmd;
+	}
+
+	// Init Struct
+	sprintf(cmd->input, "%s", "");
+	sprintf(cmd->output, "%s", "");
+	cmd->bg = false;
+	
+	char* token;
+	// Set command
+	token = strtok(string, delimit);
+	memcpy(cmd->command, token, sizeof(token));
+	
+	size_t arg_count = 0;
+	// Start setting loop
+	while(token != NULL){
+		if (strcmp(token, "<") == 0){
+			memcpy(cmd->input, strtok(NULL, delimit), sizeof(cmd->input));
+		}
+		else if (strcmp(token, ">") == 0){
+			memcpy(cmd->output, strtok(NULL, delimit), sizeof(cmd->output));
+		}
+		else{
+			cmd->args[arg_count] = token;
+			arg_count += 1;
+		}
+		token = strtok(NULL, delimit);
+	}
+	cmd->argc = arg_count;
+	if(strcmp(cmd->args[arg_count - 1],"&") == 0){
+		cmd->args[arg_count -1] = '\0';
+		cmd->bg = true;
+		if(strcmp(cmd->input, "") == 0){
+			memcpy(cmd->input, "/dev/null", sizeof(cmd->input));
+		}
+		if(strcmp(cmd->output, "") == 0){
+			memcpy(cmd->output,  "/dev/null", sizeof(cmd->output));
+		}
+	}
+	cmd->args[arg_count] = NULL;
+	return cmd;
+}
+
+void print_command(struct command* cmd){
+	printf("command: %s\n", cmd->command);
+	printf("output: %s\n", cmd->output);
+	printf("input: %s\n", cmd->input);
+	printf("background: %d\n", cmd->bg);
+	printf("args: "); for (int i = 0; cmd->args[i] != NULL; i++) printf("%s ", cmd->args[i]);
+	printf("\n");
+}
+
+//https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
+char* expand(char* string, char* pattern){
+	
+	char pid_string[PID_LIM] = {0};
+	sprintf(pid_string, "%ld", (long)getpid());
+
+	size_t new_string_size = strlen(string) + 1;
+	char* new_string = malloc(new_string_size);
+	size_t offset = 0;
+	
+	char* delim;
+	char* in = string;
+	
+	while ((delim = strstr(in, pattern))){
+		memcpy(new_string + offset, in, delim - in);
+		offset += delim - in;
+
+		in = delim + strlen(pattern);
+		
+		new_string_size = new_string_size - strlen(pattern) + strlen(pid_string);
+		new_string = realloc(new_string, new_string_size);
+
+		memcpy(new_string + offset, pid_string, strlen(pid_string));
+		offset += strlen(pid_string);
+	}
+
+	strcpy(new_string + offset, in);
+	free(string);
+	return new_string;		
+}
+
